46f6f2db4f496aabd42837d62a4af4fa82ef6738
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-01-30 11:24:44 +0100

    initial commit

---
+++ main8.js
@@ -0,0 +1,18 @@
+let array_1 = [
+  ['un', 'per', 'incatenarli.'],
+  ['Anello', 'trovarli,'],
+  ['ghermirli', 'e'],
+  ['gondor', 'mark'],
+];
+
+let array_2 = [
+  [['trovarli,']],
+  ['tu,', 'sciocchi'],
+  ['tu,', 'sciocchi', ['padron', 'Sauron']],
+  ['nel', ['fuggite', 'gandalf']],
+  [['domarli,', 'passare'], 'buio']
+];
+
+// "Un Anello per domarli, un Anello per trovarli, un Anello per ghermirli e nel buio incatenarli.“
+
+console.log(array_1[0][0], array_1[1][0], array_1[0][1], array_2[4][0][0], array_1[0][0], array_1[1][0], array_1[0][1], array_1[1][1], array_1[0][0], array_1[1][0], array_1[0][1], array_1[2][0], array_1[2][1], array_2[3][0], array_2[4][1], array_1[0][2]);
