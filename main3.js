1b0f807d284320fe11d701d758d19f0ef9e1b2b1
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-02-03 11:33:43 +0100

    terminato selfwork funz. 3

--- main3.js
+++ main3.js
@@ -1,2 +1,23 @@
-let ciao = `ciaone`
-console.log(ciao);
+function num (N) {
+    if (N > 0 & N <10) {
+        console.log(`1 cifra`);
+
+    }else if (N >= 10 & N <= 99){    
+        console.log(`2 cifre`);
+        
+    }else if (N >= 100 & N <= 999){    
+        console.log(`3 cifre`);
+        
+    }else if (N >= 1000 & N <= 9999){    
+        console.log(`4 cifre`);
+        
+    }else if (N <= 0) {
+        console.log(`numero non positivo`);
+        
+    }else{
+        console.log(`numero troppo grande`);
+    }
+}
+let N = Number(prompt(`Scegli un numero:`))
+
+num (N)
